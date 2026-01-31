89ac1bc3cabbb1e551b9036eca1ac980b0ffdc32
Kabilan N <kabilann@Kabilans-Mac-mini.local>
2026-01-31 08:04:36 +0530

    mail update

--- resources/views/emails/auth/verify-email.blade.php
+++ resources/views/emails/auth/verify-email.blade.php
@@ -3,7 +3,8 @@
 
 Hi {{ $user->name }},
 
-Welcome to {{ config('app.name') }}! To get started and compete in tournaments, please verify your email address by clicking the button below.
+Welcome to **{{ config('app.name') }}**! 🎮  
+To get started and compete in tournaments, please verify your email address by clicking the button below.
 
 @component('mail::button', ['url' => $url, 'color' => 'success'])
 Verify Email Address
@@ -11,10 +12,12 @@ Verify Email Address
 
 If you didn't create an account, you can safely ignore this email.
 
-Thanks,<br>
-{{ config('app.name') }}
+Thanks,  
+**{{ config('app.name') }} Team**
 
 @component('mail::subcopy')
-If you're having trouble clicking the "Verify Email Address" button, copy and paste the URL below into your web browser: [{{ $url }}]({{ $url }})
-@endslot
+If you're having trouble clicking the **"Verify Email Address"** button, copy and paste the URL below into your web browser:
+
+{{ $url }}
+@endcomponent
 @endcomponent
